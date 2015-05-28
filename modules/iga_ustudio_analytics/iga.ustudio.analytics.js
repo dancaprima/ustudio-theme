c0b498c8db1d2f27afb425ed3349ed9656406821
Poindexter, Malcolm <Malcolm.Poindexter@umusic.com>
2015-05-27 17:59:50 -0700

    grunt updates

--- modules/iga_ustudio_analytics/iga.ustudio.analytics.js
+++ modules/iga_ustudio_analytics/iga.ustudio.analytics.js
@@ -29,46 +29,99 @@
 			ga('send', 'event', 'outbound links', 'click', url);
 		}
 	}
-	//Track outbound links
-	$("a:external").on("click", function(e){
-		trackOutboundLink(e.target.href);
-	});
+
 	uStudio.uStudioCore.instance.registerModule({
 		name: "iga_ustudio_analytics",
 		initialize: function(configuration, events, videos) {
 			var settings = {
-				videoName: "",
-				progress: 0
+				name: "",
+				duration:0,
+				progress: 0,
+				bucket: null
 			};
-			console.log("iga_ustudio_analytics:initialize");
+			//Track outbound links
+			$(function(){
+				setTimeout(function(){
+					$("a:external").on("click", function(e){
+						trackOutboundLink(e.target.href);
+					});
+				},0);
+			});
 			//## Events
 			//### Play
 			events.subscribe("Player.playing", function() {
-				if(_gaOK()){ ga("send", "event", "uStudio Player", "play", settings.videoName); }
+				if(_gaOK()){ ga("send", "event", "uStudio Player", "play", settings.name); }
 			});
 			//### Paused
 			events.subscribe("Player.paused", function() {
-				if(_gaOK()){ ga("send", "event", "uStudio Player", "pause", settings.videoName); }
+				if(_gaOK()){ ga("send", "event", "uStudio Player", "pause", settings.name); }
 			});
 			//### Ended
 			events.subscribe("Player.ended", function() {
-				if(_gaOK()){ ga("send", "event", "uStudio Player", "end", settings.videoName); }
+				if(_gaOK()){ ga("send", "event", "uStudio Player", "end", settings.name); }
 			});
 			//### Time Update
 			events.subscribe("Player.timeupdate", function(timeEvent) {
-				var timeDiff = timeEvent.currentTime - settings.progress;
-				if(timeDiff > 10){
-					// track in 10 second intervals
-					if(_gaOK()){ ga("send", "event", "uStudio Player", "time", settings.videoName, timeDiff, true); }
-					settings.progress += timeDiff;
+				var update = settings.bucket.update(timeEvent.currentTime);
+				if(update.changed !== 0){
+					// track in buckets of 5%
+					if(_gaOK()){ ga("send", "event", "uStudio Player", "progress : "+update.bin.percentage+"%", settings.name, 1, true); }
 				}
 			});
 			//### Video Selected
 			events.subscribe("Playlist.videoSelected", function(video) {
-				settings.videoName = video.video.name;
+				settings.name = video.video.name;
+				settings.duration = video.video.duration;
+				settings.bucket = new Bucket(20, video.video.duration);// 5% buckets
 				settings.progress = 0;
-				if(_gaOK()){ ga("send", "event", "uStudio Player", "video", settings.videoName); }
+				if(_gaOK()){ ga("send", "event", "uStudio Player", "video", settings.name); }
 			});
 		}
 	});
+
+	function Bucket(count, max){
+		this.size = (max / count) | 0;// IDIV
+		var bins = this.bins = [];
+		this._bin = 0;
+		for(var i = 0; i <= count; i++){// from 0-count (0-100%)
+			bins.push({
+				index: i,
+				percentage: ((i / count) * 100) | 0,
+				start: this.size * i, // >=
+				end: this.size * (i+1)// <
+			});
+		}
+	}
+	//inherits(Bucket, EventEmitter);
+
+	Bucket.prototype.bin = function(val){
+		var bin = (val / this.size) | 0; //IDIV
+		if(bin >= this.bins.length){
+			bin = this.bins.length-1;
+		}else if (bin < 0){
+			bin = 0;
+		}
+		return this.bins[bin];
+	};
+
+	Bucket.prototype.update = function(val){
+		var compare = this.compare(val ),
+			oldBin = this.bins[this._bin];
+		this._bin = this.bin(val).index;
+		var newBin = this.bins[this._bin],
+			data = { changed: compare, bin: newBin, from: oldBin };
+		//this.trigger("update", [data]);
+		return data;
+	};
+
+	Bucket.prototype.compare = function(val){
+		var bin = this.bin(val);
+		if(this._bin === bin.index){
+			return 0;
+		}else if(bin.index > this._bin){
+			return 1;
+		}else{
+			return -1;
+		}
+	};
 })(jQuery);
